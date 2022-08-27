37e1c372c0500cac6974c5282e17ea488808cf70
Hammad Shoaib <hammadsami119@gmail.com>
2022-08-27 23:16:03 +0500

    update index.js

--- src/App.js
+++ src/App.js
@@ -2,14 +2,12 @@ import React from 'react'
 import "./style.css"
 import Header from './components/Header'
 import Meme from './components/Meme'
-import Form from './components/Form'
 
 const App = () => {
   return (
     <div>
       <Header />
       <Meme />
-      {/* <Form /> */}
     </div>
   )
 }
